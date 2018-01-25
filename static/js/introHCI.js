4cc566d0d1af0a8f059ab151f8f878d20910f87d
Shaquile Phuong <sphuong@ucsd.edu>
2018-01-25 09:58:47 -0800

    Fixed fadeToggle

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -30,7 +30,7 @@ function projectClick(e) {
     if (description.length == 0) {
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
     } else {
-    	$(containingProject).fadeToggle();
+    	$(".project-description").fadeToggle("slow", "linear");
     	//description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
     }
 }
